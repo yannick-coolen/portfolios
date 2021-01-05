fb3f6a007695b59bc60c06f59beb181b9001cb4b
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2021-01-06 00:34:57 +0100

    committed on 06-01-2021, 00:35

--- 1st_project/js/app.js
+++ 1st_project/js/app.js
@@ -2,7 +2,7 @@
 const mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = () => scrollFunction();
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
