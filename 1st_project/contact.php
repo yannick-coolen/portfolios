6c1ed7b0b7f45dbfa989df0fb476c7630104b3c9
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2021-01-06 00:24:43 +0100

    committed on 06-01-2021, 00:25

--- 1st_project/contact.php
+++ 1st_project/contact.php
@@ -7,15 +7,41 @@ require_once('include/nav.php');
     <main>
         <article>
         <section id="main-section">
+            <!-- Date -->
             <section>
-                <div class="main-container">
-                    
+                <div class="main-container-date">
+                    <p class="date">
+                        <?php $mydate = getdate(date("U"));
+                        echo "$mydate[weekday], $mydate[mday] $mydate[month],  $mydate[year]";
+                        ?>
+                    </p>
                 </div>
             </section>
-
+            <!-- Toptext -->
             <section>
-                <div>
-                    
+                
+                </div>
+            </section>
+            <!-- main-middle -->
+            <section>
+                <div class="grid-container-middle">
+                    <div class="main-left">
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                    </div>
+                    <div class="main-right">
+                        <p>text</p> 
+                    </div>
                 </div>
             </section>
         </section>
--- 1st_project/index.php
+++ 1st_project/index.php
@@ -5,18 +5,50 @@ require_once('include/nav.php');
 ?>
 <main>
     <article>
+        <!-- Main-section -->
         <section id="main-section">
+            <!-- Date -->
             <section>
-                <div class="main-container">
+                <div class="main-container-date">
                     <p class="date">
                         <?php $mydate = getdate(date("U"));
                         echo "$mydate[weekday], $mydate[mday] $mydate[month],  $mydate[year]";
                         ?>
                     </p>
-                    <div class="grid-container">
-                        <div class="main"><h2>Hello</h2></div>
-                        <div class="profile-pic"><img src="img/foto.png" alt=""></div>
+                </div>
+            </section>
+            <!-- Toptext -->
+            <section>
+                <div class="grid-container-top">
+                    <div class="main-text">
+                        <h2>Hello</h2>
                     </div>
+                    <div class="profile-pic">
+                        <img src="img/foto.png" alt="">
+                    </div>
+                </div>
+            </section>
+            <!-- main-middle -->
+            <section>
+                <div class="grid-container-middle">
+                    <div class="main-left">
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                    </div>
+                    <div class="main-right">
+                        <p>text</p> 
+                    </div>
+                </div>
             </section>
         </section>
     </article>
