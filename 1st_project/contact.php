ed18a29bea7d394578fb0b5653a65ee3f551b591
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2021-01-11 21:49:06 +0100

    Update contact.php

--- 1st_project/contact.php
+++ 1st_project/contact.php
@@ -3,9 +3,11 @@ require_once('includes/nav.php');
 ?>
 <title>Contact</title>
 </head>
+
 <body>
     <main>
         <article>
+<<<<<<< Updated upstream
         <section id="main-section">
             <!-- Date -->
             <section>
@@ -45,8 +47,40 @@ require_once('includes/nav.php');
 
                     </form>
                 </div>
+=======
+            <section id="main-section">
+                <!-- Date -->
+                <section>
+                    <div class="main-container-date">
+                        <p class="date">
+                            <?php $mydate = getdate(date("U"));
+                            echo "$mydate[weekday], $mydate[mday] $mydate[month],  $mydate[year]";
+                            ?>
+                        </p>
+                    </div>
+                </section>
+                <!-- Toptext -->
+                <section>
+
+                    </div>
+                </section>
+                <!-- main-middle -->
+                <section>
+                    <form action="" method="post">
+                        <div class="grid-container-middle">
+                            <div class="main-left">
+                                <label for="">Fullname</label>
+                                <input type="text" placeholder="Please, in your full name">
+                                
+                            </div>
+                            <div class="main-right">
+                                <p>text</p>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+>>>>>>> Stashed changes
             </section>
-        </section>
         </article>
     </main>
     <?php
